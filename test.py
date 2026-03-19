78e3d2a5661ab4b17322cadb942dce1d7fa630da
Marina W <mwsppk@icloud.com>
2026-03-19 08:22:04 +0100

    Update with linter correction test

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
+"""Test function to verify whether the Roman numeber was calculated correctly"""
 from utils import roem
 
 def test_roem(zahl):
-    if zahl <= 0 or zahl > 3999:
-        print("Bitte Zahl zwischen 1 und 3999 eingeben.")
-        return
+    """function to test the converter."""
 
     tausender = zahl // 1000
     hunderter = (zahl % 1000) // 100
     zehner = (zahl % 100) // 10
     einer = zahl % 10
 
-    print(f"Hunderter: {roem(hunderter*100)}", f"tausender: {roem(tausender*1000)}", f"Zehner: {roem(zehner*10)}", f"Einer: {roem(einer)}", f"Gesamt: {roem(zahl)}")
+    print(f"Hunderter: {roem(hunderter*100)}", f"tausender: {roem(tausender*1000)}",
+          f"Zehner: {roem(zehner*10)}", f"Einer: {roem(einer)}", f"Gesamt: {roem(zahl)}")
