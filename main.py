783565620866e19a9751bb52db8227d15ffb31c1
Marina W <mwsppk@icloud.com>
2026-03-19 08:21:08 +0100

    update with linter Correction

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# variablen in römisch
+"""Calling the functions to perform the conversion"""
+
+from test import test_roem
 from utils import roem
-from test import *
 
 zahl=int(input("Bitte gebe eine Zahl ein die als Römische Zahl dargestellt werden soll:"))
 print("info: I=5, V=5, X=10, L=50, C=100, D=500, M=1000")
 print(roem(zahl))
 print(test_roem(zahl))
-
-#test
-
-
