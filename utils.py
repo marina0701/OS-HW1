8daeeb5ac34d91ed9c79e7882bd020cd006a0392
Marina W <mwsppk@icloud.com>
2026-03-19 08:22:31 +0100

    update with linter correction

--- utils.py
+++ utils.py
@@ -1,9 +1,24 @@
+"""function to convert arabic numbers to roman number format."""
 def roem(i):
-    zif=[(1000,"M"),(900,"CM"),(500,"D"),(400,"CD"),(100,"C"),(90,"XC"),(50,"L"),(40,"XL"),(10,"X"),(9,"IX"),(5,"V"),(4,"IV"),(1,"I")]
+    """function for converting"""
+
+    zif=[(1000,"M"),
+         (900,"CM"),
+         (500,"D"),
+         (400,"CD"),
+         (100,"C"),
+         (90,"XC"),
+         (50,"L"),
+         (40,"XL"),
+         (10,"X"),
+         (9,"IX"),
+         (5,"V"),
+         (4,"IV"),
+         (1,"I")]
     erg=""
 
-    for ZErg, roembuch in zif:
-        while i>= ZErg:
-           erg+=roembuch
-           i-=ZErg
+    for erg2, roembuch in zif:
+        while i>= erg2:
+            erg+=roembuch
+            i-=erg2
     return erg
